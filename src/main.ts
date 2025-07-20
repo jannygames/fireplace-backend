c01b988022c2a1a9bc9676f0d1ec1150b4de1b2e
jannygames <jamolisdev@gmail.com>
2025-07-21 00:53:45 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Fireplace API')
+    .setDescription('The Fireplace API documentation')
+    .setVersion('1.0')
+    .addTag('fireplace')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 5001);
+}
+bootstrap().catch((error) => console.error(error));
